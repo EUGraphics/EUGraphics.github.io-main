62daf615184837cd1efdb6de03fce63dc16b2a53
Peter Bækgaard <pete372b@gmail.com>
2017-04-20 15:36:23 +0200

    first snap
    not finished yet at all, just some files to start out with and see if
    stuff works.

---
+++ assets/js/enable-scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(function() {
+    $("#main").onepage_scroll({
+       sectionContainer: "page",
+       easing: "ease",
+       animationTime: 1000,
+       pagination: false,
+       updateURL: false,
+    });
+});
+});
---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var $readmore = $(".readmore-btn");
+  $readmore.on("click", function() {
+  $("#main").moveTo(3);
+});
+});
