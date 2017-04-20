27b92d7eb60e1f4471058f212105b2b8426ed900
Peter Bækgaard <pete372b@gmail.com>
2017-04-20 21:33:18 +0200

    VARIOUS FIXES AND NEW STUFF
    FIXED LANDING PAGE BUTTONS NOT WORKING
    
    ADDED CONTACT PAGE
    
    FIXED LAYOUT BEING WEIRD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  var $readmore = $(".readmore-btn");
+  var $readmore = $("#rm-readmore");
+  var $contact = $(".ct-btn");
   $readmore.on("click", function() {
-  $("#main").moveTo(3);
-});
+  $("#main").moveTo(2);
+  });
+
+  $contact.on("click", function() {
+    $("#main").moveTo(4);
+    });
 });
